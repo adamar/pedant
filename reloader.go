ab40600e10dd1bbc8d390f80b1bd434b6236da0b
Andrew Martin <a.a.d.martin@gmail.com>
2014-08-25 15:07:56 +1000

    First Commit

---
+++ reloader.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"code.google.com/p/go.exp/inotify"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type State struct {
+	json  []byte
+	valid bool
+}
+
+func main() {
+
+	fileName := "/tmp/file.json"
+
+	status := make(chan *State)
+
+	go func() {
+		for state := range status {
+			log.Print(state)
+		}
+	}()
+
+	trackFile(fileName, status)
+
+}
+
+func errHandler(err error) {
+
+	if err != nil {
+		log.Print(err)
+	}
+
+}
+
+func validateJSON(jdata []byte) bool {
+	var jmap map[string]interface{}
+	return json.Unmarshal(jdata, &jmap) == nil
+}
+
+func trackFile(filename string, status chan<- *State) {
+
+	watcher, err := inotify.NewWatcher()
+	errHandler(err)
+
+	err = watcher.Watch(filename)
+	errHandler(err)
+
+	for {
+		select {
+		case ev := <-watcher.Event:
+			if ev.Mask == 2 {
+				log.Print("file changed")
+				jsondata, err := ioutil.ReadFile(filename)
+				errHandler(err)
+
+				status <- &State{json: jsondata, valid: validateJSON(jsondata)}
+			}
+		}
+	}
+
+}
